3fbae7c9b349a87f8fc8d4d2ce106ecda6d75a9a
adityap5 <aditya29pippal@gmail.com>
2023-10-15 19:05:02 +0530

    1

--- app/page.js
+++ app/page.js
@@ -1,13 +1,25 @@
-import React from 'react'
+"use client"
+import React,{useState} from 'react'
 
 const page = () => {
+  const [Title, setTitle] = useState("");
+  const [desc, setdesc] = useState("")
+  const submitHandler = (e) => {
+   e.preventDefault();
+   console.log(Title);
+   setTitle("");
+   setdesc("");
+  }
   return (
   <>
   <h1>todo-list</h1>
-  <form>
-    <input className="in" type="text" placeholder="Enter Title here" />
-    <input className="in" type="text" placeholder="Enter Description here" />
-    <button className="btn1">Add Item</button>
+  <form onSubmit={submitHandler}>
+    <input className="in" type="text" value={Title} onChange={(e)=>{
+      setTitle(e.target.value)
+    }} placeholder="Enter Title here" />
+    <input className="in" type="text"value={desc} onChange={(e)=>{
+      setdesc(e.target.value)}} placeholder="Enter Description here" />
+    <button className="btn1">Add Task</button>
   </form>
   </>
   )
