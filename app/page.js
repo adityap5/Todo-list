7ea9bcb0d0032472746664b0a66d7d78aa1be724
adityap5 <aditya29pippal@gmail.com>
2023-10-15 18:31:24 +0530

    input

--- app/page.js
+++ app/page.js
@@ -5,7 +5,9 @@ const page = () => {
   <>
   <h1>todo-list</h1>
   <form>
-    <input className="in" type="text" placeholder="Enter your title here" />
+    <input className="in" type="text" placeholder="Enter Title here" />
+    <input className="in" type="text" placeholder="Enter Description here" />
+    <button className="btn1">Add Item</button>
   </form>
   </>
   )
